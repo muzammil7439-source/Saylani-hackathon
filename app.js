bc0d86218c708d65e8985227af59b1ba5dcfc6ff
muzammil <muzammil7439@gmail.com>
2025-12-21 04:56:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const dialogflow = require('@google-cloud/dialogflow');
+const { WebhookClient, Suggestion } = require('dialogflow-fulfillment');
+const express = require("express")
+const cors = require("cors");
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post("/webhook", async (req, res) => {
+    var id = (res.req.body.session).substr(43);
+    console.log(id)
+    const agent = new WebhookClient({ request: req, response: res });
+
+    function hi(agent) {
+        console.log(`intent  =>  hi`);
+        agent.add("server is working");
+    }
+
+    function sendNotes(agent) {
+        const { number , date , email} = agent.parameters;
+       agent.add("Your notes have been sent successfully!");
+    }
+    let intentMap = new Map();
+    intentMap.set('hi', hi); 
+    intentMap.set('sendNotes', sendNotes);
+    agent.handleRequest(intentMap);
+})
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
